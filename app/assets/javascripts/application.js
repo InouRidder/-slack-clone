1d7793a0c2c1338ea26f763b448764bbf7c35709
Inou Ridder <inouridder@gmail.com>
2018-05-21 19:15:50 +0800

    Front end scroll box messages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,7 @@
 //= require rails-ujs
 //= require_tree .
+
+function scrollToLastMessage() {
+  const messages = document.getElementById('messages');
+  messages.children[messages.children.length - 1].scrollIntoView();
+}
