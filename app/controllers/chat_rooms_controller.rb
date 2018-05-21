839c0e1109c3bd4bb23dcac2ee4ce4d7acfdf2b7
Inou Ridder <inouridder@gmail.com>
2018-05-21 18:58:26 +0800

    Initial front end

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -6,5 +6,9 @@ class ChatRoomsController < ApplicationController
   def show
     @chat_room = ChatRoom.find(params[:id])
     @message = Message.new
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,5 @@ Rails.application.routes.draw do
   end
 
   mount ActionCable.server => "/cable"
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
